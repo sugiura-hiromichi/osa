3343e6932a423b48a138b2d7de79ba9bb9fd31b5
sugiura-hiromichi <sugiura130418@icloud.com>
2023-02-01 17:42:17 +0900

    fix(sh_cmd) updated

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ fn main() {
 	let mut arg = std::env::args();
 	arg.next();
 	let xec_nam = arg.next().unwrap();
-	sh_cmd!(format!("{OSA_BIN}{xec_nam}"), arg);
+	let mut args = vec![];
+	arg.for_each(|a| args.push(a,),);
+	sh_cmd!(format!("{OSA_BIN}{xec_nam}"), args).unwrap();
 }
